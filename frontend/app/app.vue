21179c6896806a0cf2a5417f32d525b6fc308951
Frank Rousseau <frank@cg-wire.com>
2025-12-29 00:21:04 +0100

    [frontend] Init Nuxt app

---
+++ frontend/app/app.vue
@@ -0,0 +1,34 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Ticket plugin'
+const description = 'Ticket systems to add issue tracking to Kitsu tasks.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <UMain>
+      <NuxtPage />
+    </UMain>
+  </UApp>
+</template>
