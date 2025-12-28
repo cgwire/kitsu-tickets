69a51f23c247641a574700773fc5b548c6183c34
Frank Rousseau <frank@cg-wire.com>
2025-12-29 00:20:02 +0100

    [frontend] Add support for api querying

---
+++ frontend/app/composables/useKitsu.js
@@ -0,0 +1,29 @@
+import kitsuClient from 'kitsu-client-js'
+
+let kitsuClientInstance = null
+
+export const useKitsu = () => {
+  const apiBaseUrl = '/api'
+
+  if (!kitsuClientInstance) {
+    console.log('Creating client', kitsuClient)
+    kitsuClientInstance = kitsuClient.createClient(apiBaseUrl)
+  }
+
+  return {
+    client: kitsuClientInstance,
+
+    context: (email, password) => {
+      return kitsuClientInstance.context(email, password)
+    },
+    getOpenProductions: () => {
+      return kitsuClientInstance.getOpenProductions()
+    },
+
+    fetchTickets: (productionId, episodeId) => {
+      return kitsuClientInstance.get('/plugins/tickets/tickets')
+    },
+
+  }
+}
+
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  ssr: false, // Disable server-side rendering (SPA mode)
+  static: true,
+
+  app: {
+    baseURL: process.env.NODE_ENV !== 'development' ? '/api/plugins/tickets/frontend/' : ''
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
